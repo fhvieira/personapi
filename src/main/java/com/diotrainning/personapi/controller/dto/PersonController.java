ba53f61ca21a46f2f30cbe0121147d1c823919b0
Fernando H Vieira <fhvieira@hotmail.com>
2021-05-07 15:06:46 -0300

    README.md file created

--- src/main/java/com/diotrainning/personapi/controller/dto/PersonController.java
+++ src/main/java/com/diotrainning/personapi/controller/dto/PersonController.java
@@ -4,6 +4,7 @@ import com.diotrainning.personapi.dto.MessageResponseDTO;
 import com.diotrainning.personapi.dto.PersonDTO;
 import com.diotrainning.personapi.exception.PersonNotFoundException;
 import com.diotrainning.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
-    private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+    private final PersonService personService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
